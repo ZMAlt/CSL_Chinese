b75604089edef3bf49f0b10f75df9563cfafdda2
Mao Zhou <maozhougeo@gmail.com>
2024-09-19 22:34:58 +1000

    update

--- src/CSL_Chinese.jl
+++ src/CSL_Chinese.jl
@@ -1,5 +1,7 @@
 module CSL_Chinese
 
+using Books
+
 using Reexport: @reexport
 
 using Reexport: @reexport
@@ -13,7 +15,6 @@ using Reexport: @reexport
 This function is called during CI.
 """
 function build()
-    println("Building Example Book")
     # To avoid publishing broken websites.
     fail_on_error = true
     gen(; fail_on_error)
